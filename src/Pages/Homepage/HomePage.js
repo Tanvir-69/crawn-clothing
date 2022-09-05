6b39749af6182d8120c07b26acd485e9a929b211
Tanvir-69 <tanvirhasan00@gmail.com>
2022-09-06 03:26:12 +0600

    part 1

---
+++ src/Pages/Homepage/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Homepage.scss';
+
+import Directory from '../../Components/Directory-menu/DirectoryMenu';
+
+const HomePage =()=>(
+    <div className='homepage'>
+        <Directory /> 
+    </div>
+)
+
+export default HomePage;
