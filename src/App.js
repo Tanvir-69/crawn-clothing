d75ac1d0dbfb91d643ef1546d31f636171e58a1d
Tanvir-69 <tanvirhasan00@gmail.com>
2022-09-08 13:17:51 +0600

    adding a custom header file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Routes, Route,Link } from 'react-router-dom';
 
 import HomePage from './Pages/Homepage/HomePage';
 import ShopPage from './Pages/Shoppage/shoppage';
+import Header from './Components/Header/Header';
 
 
 const HatsPage =()=>(
@@ -19,7 +20,7 @@ const App = () => {
   return (
     <div className="App">
       {/* <ShopPage /> */}
-      
+      <Header />
       <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='/shop' element={<ShopPage />} />
